2fac595e3b84b20b2476646a11635dde30c086c5
Ahamad <alishaahamadsayed@gmail.com>
2021-08-12 09:05:09 -0500

    Adding kinesis producer code

--- src/main/java/com/example/demo/KinesisProducer.java
+++ src/main/java/com/example/demo/KinesisProducer.java
@@ -1,16 +1,26 @@
 package com.example.demo;
 
-import com.example.demo.model.Facility;
 import com.example.demo.model.FacilityRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Function;
 
 @Component
+@EnableBinding(Processor.class)
 public class KinesisProducer implements Function<FacilityRequest, String> {
+
+    @Autowired
+    private Processor processor;
 
     @Override
     public String apply(FacilityRequest facilityRequest) {
+        System.out.println("pushing the facility request to kinesis stream");
+        processor.output().send(MessageBuilder.withPayload(facilityRequest.toString()).build());
+        System.out.println("Successfully pushed the data to kinesis stream");
         return facilityRequest.toString();
     }
 }
